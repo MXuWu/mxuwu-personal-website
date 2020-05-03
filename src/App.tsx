3d54d94ebb73e453f857e9f3d7da2fcc46c4a7f6
Michael Wu <michael.wu@westpac.com.au>
2020-05-03 21:34:08 +1000

    typing animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [text, updateText] = useState("");
+  const [ind, updateInd] = useState(0);
+  const [cursorClass, updateCursorClass] = useState("");
+  const name = "Michael Wu\nProgrammer\nArtistic Savant";
+
+  function writeText(fullStr: string) {
+    updateText((text) => {
+      if (ind < fullStr.length) {
+        updateInd(ind + 1);
+        return text + fullStr[ind];
+      }
+      return text;
+    });
+  }
+
+  useEffect(() => {
+    console.log("effect", ind);
+    setTimeout(() => writeText(name), 100);
+  });
+
+  useEffect(() => {
+    if (text === name) updateCursorClass("cursor-blink");
+  }, [text]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to my bloody website
-        </p>
+      <header className="App-header hero">
+        <pre style={{ fontWeight: 600 }}>
+          {text}
+          <span className={`cursor ${cursorClass}`}></span>
+        </pre>
       </header>
+      <div className="info">
+        <div className="info-panel">
+          <div>Contact</div>
+          <div>Mail Github LinkedIn</div>
+        </div>
+      </div>
     </div>
   );
 }
